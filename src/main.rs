ae639c9c9a2cbe548192cc9fa44a849dfa4f524c
azur <natapat.samutpong@gmail.com>
2022-09-26 18:13:40 +0700

    Separate panes, render before process

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,110 @@ async fn main() {
         let x_offset = x_center_offset + x_pan_offset;
         let y_offset = y_center_offset + y_pan_offset;
 
+        // -------------------- [ Rendering ] --------------------
+
+        clear_background(Color::from_rgba(10, 12, 14, 255));
+        let grid_color = Color::from_rgba(68, 81, 94, 255);
+
+        // Drawing grid lines
+        for x in 0..=grid.width {
+            draw_line(
+                // from
+                x_offset + x as f32 * grid_cell_size as f32,
+                y_offset,
+                // to
+                x_offset + x as f32 * grid_cell_size as f32,
+                y_offset + grid.height as f32 * grid_cell_size as f32,
+                1.0, // width
+                grid_color
+            ); 
+        }
+
+        for y in 0..=grid.height {
+            draw_line(
+                // from
+                x_offset,
+                y_offset + y as f32 * grid_cell_size as f32,
+                // to
+                x_offset + grid.width as f32 * grid_cell_size as f32,
+                y_offset + y as f32 * grid_cell_size as f32,
+                1.0, // width
+                grid_color
+            ); 
+        }
+
+        // Rendering grid cells
+        for x in 0..grid.width {
+            for y in 0..grid.height {
+                let cell_color = grid.get(x, y);
+                if cell_color[3] != 0 {
+                    draw_rectangle(
+                        x_offset + x as f32 * grid_cell_size as f32,
+                        y_offset + y as f32 * grid_cell_size as f32,
+                        grid_cell_size as f32,
+                        grid_cell_size as f32,
+                        Color::from_rgba(cell_color[0], cell_color[1], cell_color[2], cell_color[3]),
+                    );
+                }
+            }
+        }
+
+        // Rendering UI
+        egui_macroquad::ui(|ctx| {
+            let colors = egui::Window::new("Colors").show(ctx, |ui| {
+                use egui::{
+                    FontFamily::Proportional,
+                    FontId
+                };
+                use egui::TextStyle::*;
+
+                let mut style = (*ctx.style()).clone();
+                style.text_styles = [
+                    (Heading, FontId::new(24.0, Proportional)),
+                    (Body, FontId::new(20.0, Proportional)),
+                    (Monospace, FontId::new(20.0, Proportional)),
+                    (Button, FontId::new(20.0, Proportional)),
+                    (Small, FontId::new(20.0, Proportional)),
+                ].into();
+                ctx.set_style(style);
+
+                ui.label("Colors");
+                ui.horizontal(|ui| {
+                    let mut c = egui::Color32::from_rgb_additive(
+                        current_color[0],
+                        current_color[1],
+                        current_color[2],
+                    );
+                    ui.color_edit_button_srgba(&mut c);
+                    current_color = [c.r(), c.g(), c.b(), 255];
+                });
+            });
+
+            let grid_actions = egui::Window::new("Grid Actions").show(ctx, |ui| {
+                ui.label("Clear grid");
+                if ui.button("Clear").clicked() {
+                    grid.clear();
+                }
+            });
+
+            // Check if the GUI is using pointer
+            // so that it blocks the mouse from drawing
+            [
+                colors,
+                grid_actions
+            ].iter().for_each(|pane| {
+                if let Some(pane) = pane {
+                    if pane.response.ctx.is_using_pointer() {
+                        on_gui = true;
+                    } else {
+                        on_gui = false;
+                    }
+                }
+            });
+        });
+
+        egui_macroquad::draw();
+
         // -------------------- [ Inputs ] --------------------
 
         // Check if not interacting with GUI
@@ -98,102 +202,6 @@ async fn main() {
                 (x_pan_pos, y_pan_pos) = (x, y);
             }
         }
-
-        // -------------------- [ Rendering ] --------------------
-
-        clear_background(Color::from_rgba(10, 12, 14, 255));
-
-        // Drawing grid lines
-        for x in 0..=grid.width {
-            draw_line(
-                // from
-                x_offset + x as f32 * grid_cell_size as f32,
-                y_offset,
-                // to
-                x_offset + x as f32 * grid_cell_size as f32,
-                y_offset + grid.height as f32 * grid_cell_size as f32,
-                1.0, // width
-                Color::from_rgba(255, 255, 255, 255),
-            );
-        }
-
-        for y in 0..=grid.height {
-            draw_line(
-                // from
-                x_offset,
-                y_offset + y as f32 * grid_cell_size as f32,
-                // to
-                x_offset + grid.width as f32 * grid_cell_size as f32,
-                y_offset + y as f32 * grid_cell_size as f32,
-                1.0, // width
-                Color::from_rgba(255, 255, 255, 255),
-            );
-        }
-
-        // Rendering grid cells
-        for x in 0..grid.width {
-            for y in 0..grid.height {
-                let cell_color = grid.get(x, y);
-                if cell_color[3] != 0 {
-                    draw_rectangle(
-                        x_offset + x as f32 * grid_cell_size as f32,
-                        y_offset + y as f32 * grid_cell_size as f32,
-                        grid_cell_size as f32,
-                        grid_cell_size as f32,
-                        Color::from_rgba(cell_color[0], cell_color[1], cell_color[2], cell_color[3]),
-                    );
-                }
-            }
-        }
-
-        // Rendering UI
-        egui_macroquad::ui(|ui_ctx| {
-            let tools = egui::Window::new("Tools").show(ui_ctx, |ui| {
-                use egui::{
-                    FontFamily::Proportional,
-                    FontId
-                };
-                use egui::TextStyle::*;
-
-                let mut style = (*ui_ctx.style()).clone();
-                style.text_styles = [
-                    (Heading, FontId::new(24.0, Proportional)),
-                    (Body, FontId::new(20.0, Proportional)),
-                    (Monospace, FontId::new(20.0, Proportional)),
-                    (Button, FontId::new(20.0, Proportional)),
-                    (Small, FontId::new(20.0, Proportional)),
-                ].into();
-                ui_ctx.set_style(style);
-
-                ui.label("Colors");
-                ui.horizontal(|ui| {
-                    let mut c = egui::Color32::from_rgb_additive(
-                        current_color[0],
-                        current_color[1],
-                        current_color[2],
-                    );
-                    ui.color_edit_button_srgba(&mut c);
-                    current_color = [c.r(), c.g(), c.b(), 255];
-                });
-
-                ui.label("Clear grid");
-                if ui.button("Clear").clicked() {
-                    grid.clear();
-                }
-            });
-
-            // Check if the GUI is using pointer
-            // so that it blocks the mouse from drawing
-            if let Some(u) = tools {
-                if u.response.ctx.is_using_pointer() {
-                    on_gui = true;
-                } else {
-                    on_gui = false;
-                }
-            }
-        });
-
-        egui_macroquad::draw();
 
         next_frame().await
     }
